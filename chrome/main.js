9146e553b22118a3c541810fed4c1ea57764cfda
Paul Repka <repkap11@gmail.com>
2017-12-02 02:02:45 -0500

    Chrome can read the database
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- chrome/main.js
+++ chrome/main.js
@@ -1,6 +1,20 @@
 send2Phone = function(word) {
   var query = btoa(word.linkUrl);
-  chrome.tabs.create({url: "http://api.repkam09.com/api/rep2phone/" + query});
+  var userId = firebase.auth().currentUser.uid;
+  firebase.database()
+      .ref('/user_group/users/' + userId)
+      .once('value')
+      .then(function(userSnapshot) {
+        console.log(userSnapshot.val());
+        userSnapshot.child('devices').forEach(function(deviceSnapshot) {
+          console.log("Got Device!");
+          var deviceID = deviceSnapshot.key;
+          console.log(deviceID);
+          chrome.tabs.create(
+              {url: "http://api.repkam09.com/api/rep2phone/" + deviceID});
+        });
+
+      });
 };
 
 chrome.contextMenus.create({
