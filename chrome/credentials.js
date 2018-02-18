a9e82dd61fab26ff201064857152f6bbae050c3c
Paul Repka <repkap11@gmail.com>
2018-02-18 17:43:28 -0500

    Fix sign in maybe
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- chrome/credentials.js
+++ chrome/credentials.js
@@ -83,10 +83,11 @@ function startAuth(interactive) {
       firebase.auth().signInWithCredential(credential).catch(function(error) {
         // The OAuth token might have been invalidated. Lets' remove it from
         // cache.
-        if (error.code === 'auth/invalid-credential') {
+        console.log('Auto Error why:',error.code);
+        //if (error.code === 'auth/invalid-credential') {
           chrome.identity.removeCachedAuthToken(
               {token: token}, function() { startAuth(interactive); });
-        }
+        //}
       });
     } else {
       console.error('The OAuth Token was null');
